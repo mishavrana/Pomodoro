ca169014ead5147cb92c951e93a8ba946022bb72
Mykhailo <misha.vrana@gmail.com>
2023-02-07 00:35:15 +0200

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Misha Vrana on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
